dc09786ea55cf6baf4cb12fa80306a29c14f4f75
Kyle Stang <kylestang@users.noreply.github.com>
2022-12-13 23:30:48 -0800

    day 6

---
+++ day6.clj
@@ -0,0 +1,11 @@
+
+(defn part-1 [input]
+  (println (+ (count (take-while #(not= (count (distinct %)) 4) (partition 4 1 input))) 4)))
+
+(defn part-2 [input]
+  (println (+ (count (take-while #(not= (count (distinct %)) 14) (partition 14 1 input))) 14)))
+
+(defn start [] 
+  ((juxt part-1 part-2) (slurp "inputs/day6")))
+
+(start)
